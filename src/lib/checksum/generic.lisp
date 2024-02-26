2ceb2633afa5a3fe13f047d14b6b4dc67aba7026
Jesse Bouwman <jesse.bouwman@proton.me>
2024-02-25 18:05:24 -0800

    work in progress

---
+++ src/lib/checksum/generic.lisp
@@ -0,0 +1,11 @@
+(defpackage #:lib.checksum.generic
+  (:use #:cl)
+  (:export
+   #:checksum
+   #:update))
+
+(in-package #:lib.checksum.generic)
+
+(defgeneric update (state buffer start count))
+
+(defgeneric checksum (state))
