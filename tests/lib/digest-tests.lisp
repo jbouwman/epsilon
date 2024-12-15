75eba3e423157d24f3dc2efb42767be3eb3e58f9
Jesse Bouwman <jesse.bouwman@pm.me>
2024-12-15 14:23:29 -0800

    code no longer derives from 'fiasco'

--- tests/lib/digest-tests.lisp
+++ tests/lib/digest-tests.lisp
@@ -4,7 +4,7 @@
 
 (in-package #:epsilon.lib.digest.tests)
 
-;; shasum -a 256 tests/data/shilling.txt
+;; shasum -a 256 tests/lib/shilling.txt
 ;; b9ed5c0ed11f350f7247678de2d16418b69c7f2ce00eabea7a8990889e54faa5  tests/data/shilling.txt
 
 (deftest sha-2 ()
