88a760e7b458442b6835f250c33acf8f3f768f16
Jesse Bouwman <jesse.bouwman@pm.me>
2024-12-25 16:33:33 -0600

    handle error in test setup

--- etc/test.lisp
+++ etc/test.lisp
@@ -1,8 +1,11 @@
-(require :asdf)
-
-(load "epsilon.asd")
-
-(asdf:load-system "epsilon/tests")
+(handler-case
+    (progn
+      (require :asdf)
+      (load "epsilon.asd")
+      (asdf:load-system "epsilon/tests"))
+  (error (condition)
+    (format *error-output "~A~%" condition)
+    (sb-debug:print-backtrace :stream *error-output*)))
 
 (sb-posix:exit (if (epsilon.tool.test:run-success-p (epsilon.tool.test:run-tests))
                    0
