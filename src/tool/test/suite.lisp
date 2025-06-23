cc46592afd9cdd2d2fca5b4bb97e2d99978582d7
Jesse Bouwman <jesse.bouwman@pm.me>
2025-06-22 18:09:36 -0700

    enable junit reporting

--- src/tool/test/suite.lisp
+++ src/tool/test/suite.lisp
@@ -5,6 +5,7 @@
   (:shadow
    condition)
   (:local-nicknames
+   (common epsilon.tool.common)
    (f epsilon.lib.function)
    (map epsilon.lib.map)
    (set epsilon.lib.set)
@@ -22,7 +23,6 @@
    assertions
    condition
    status
-   event
    select
    run
    pass
@@ -30,14 +30,14 @@
    skip
    errors
    failures
-   skipped))
+   skipped
+   list-suites
+   suite-tests))
 
 (in-package epsilon.tool.test.suite)
 
 (defvar *test* nil
   "Dynamically bound to current test result during execution")
-
-(defgeneric event (reporter event-type event-data))
 
 (defclass test-node ()
   ((children :initform map:+empty+
@@ -143,8 +143,8 @@ Returns the leaf node for package-name."
                      :accessor abort-on-failure
                      :initarg :abort-on-failure)))
 
-(defmethod event ((run test-run) type data)
-  (event (reporter run) type data))
+(defmethod common:event ((run test-run) type data)
+  (common:event (reporter run) type data))
 
 (defun failures (run)
   "Return list of failed test results"
@@ -257,6 +257,23 @@ Returns the leaf node for package-name."
 (defun run-success-p (run)
   (zerop (+ (length (failures run))
             (length (errors run)))))
+
+(defun list-suites (test-run)
+  "Return a sorted sequence of package names that have at least one test result"
+  (sort (remove-duplicates 
+         (map:vals (map:map (lambda (result)
+                              (package-name (symbol-package (test result))))
+                            (tests test-run)))
+         :test #'string=)
+        #'string<))
+
+(defun suite-tests (test-run suite-name)
+  "Return all test results for the given suite (package) name, sorted by test name"
+  (sort (map:vals (map:filter (lambda (name result)
+                                (string= suite-name 
+                                         (package-name (symbol-package (test result)))))
+                              (tests test-run)))
+        #'string< :key (lambda (result) (symbol-name (test result)))))
 
 (define-condition test-failure (error)
   ((message :initarg :message :reader failure-message)
