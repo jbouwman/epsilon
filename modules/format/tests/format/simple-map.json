1db9ab785b5db1aa8ab80d39bd1c2c8420ed3b79
Jesse Bouwman <jesse.bouwman@pm.me>
2025-08-10 19:25:49 -0700

    trim incomplete modules

--- modules/format/tests/format/simple-map.json
+++
@@ -1,4 +0,0 @@
-{
-  "name": "test",
-  "value": 42
-}
--- modules/monitor/module.lisp
+++
@@ -1,8 +0,0 @@
-(:name "epsilon.monitor"
- :version "0.1.0"
- :author "Jesse Bouwman"
- :description "Structured Process Communication Framework for real-time monitoring and progress tracking"
- :requires ("epsilon.core" "epsilon.test")
- :sources ("src")
- :tests ("tests")
-)
