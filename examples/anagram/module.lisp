51258aaa0d0ea360cb8237a507332b6bd5931da6
Jesse Bouwman <jesse.bouwman@pm.me>
2025-08-20 04:30:38 +0000

    moved examples

--- examples/anagram/module.lisp
+++
@@ -1,8 +0,0 @@
-(:name "anagram"
- :version "1.0.0"
- :author "Jesse Bouwman"
- :description "A web service that generates anagrams"
- :requires ("epsilon.json"
-            "epsilon.http"
-            "epsilon.web"
-            "epsilon.net"))
